a24987b3700f4cc48efac32d22a7e3d4db0f749d
bancodeherramientassenaciaa <bancodeherramientassenaciaa@gmail.com>
2024-11-13 11:00:59 -0500

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',  // Asegura que los archivos se coloquen en el directorio `dist`
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
-})
+});

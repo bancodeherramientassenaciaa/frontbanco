3cf60ec686ce9f4f5fe7f4daed61ffb25e44ccde
bancodeherramientassenaciaa <bancodeherramientassenaciaa@gmail.com>
2024-11-18 09:09:42 -0500

    ancho del componente de lista

--- src/components/listas/ListComponent.jsx
+++ src/components/listas/ListComponent.jsx
@@ -40,7 +40,7 @@ const ListComponent = ({
 
             {/* Tabla genérica con scroll vertical y horizontal */}
             <div className="overflow-x-auto">
-                <div className="max-h-[500px] max-w-[700px] overflow-y-auto overflow-x-auto">
+                <div className="max-h-[500px] max-w-[1000px] overflow-y-auto overflow-x-auto">
                     <table className="min-w-full bg-white border border-black">
                         <thead>
                             <tr>
